5c74b49619c0b0a455564da0600f27d8f9ca9947
samover <samuel.overloop@gmail.com>
2015-11-28 20:49:55 +0000

    add factory girl

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    email 'pjackson@gmail.com'
+    password 'password'
+    password_confirmation 'password'
+  end
+end
---
+++ spec/features/comments_features_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'features for commenting on posts' do
+  context 'viewing comments' do
+    scenario 'any user can see comments on a post' do
+    end
+  end
+
+  context 'creating comments' do
+  end
+end
