47e388749aa04e1899341ddb6487ad4537929408
samover <samuel.overloop@gmail.com>
2015-11-28 15:21:26 +0000

    model: post requires an image

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,5 +10,11 @@ describe Post, type: :model do
       Post.create(caption: 'My first upload', image: @photo)
       expect(Post.all.count).to eq 1
     end
+
+    it 'does not allow a post without a photo' do
+      post = Post.new(caption: 'An invalid post')
+      expect(post).to have(1).error_on :image
+      expect(post).not_to be_valid
+    end
   end
 end
