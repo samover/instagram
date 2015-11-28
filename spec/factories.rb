b757951af5251d9dd3c1c3d1a64d70cc10bed43e
samover <samuel.overloop@gmail.com>
2015-11-28 21:25:33 +0000

    add comment model and association with user and post

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,4 +4,13 @@ FactoryGirl.define do
     password 'password'
     password_confirmation 'password'
   end
+
+  factory :post do
+    caption 'An example post'
+    image { fixture_file_upload('files/first_upload.jpg', 'image/jpg') }
+  end
+
+  factory :comment do
+    text 'This is a comment'
+  end
 end
