849460284d50db9753e674a2e14f3c9ce545ee6e
samover <samuel.overloop@gmail.com>
2015-11-28 21:25:33 +0000

    add comment model and association with user and post

--- spec/features/comments_features_spec.rb
+++ spec/features/comments_features_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 describe 'features for commenting on posts' do
+  let(:user) { create(:user) }
+  let(:post) { create(:post, user_id: user.id) }
+  let(:comment) { create(:comment, user_id: user.id, post_id: post.id)}
+
   context 'viewing comments' do
+    before { Comment.create(text: 'This is a comment')}
     scenario 'any user can see comments on a post' do
+      visit '/posts'
+      expect(page).to have_content 'This is a comment'
     end
   end
 
