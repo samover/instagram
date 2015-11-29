47f7eb3e8c28f5073557d0471eda4bf6f7398dc2
samover <samuel.overloop@gmail.com>
2015-11-29 00:57:00 +0000

    a user can comment on a post

--- spec/features/comments_features_spec.rb
+++ spec/features/comments_features_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 describe 'features for commenting on posts' do
-  let(:user) { create(:user) }
-  let(:post) { create(:post, user_id: user.id) }
-  let(:comment) { create(:comment, user_id: user.id, post_id: post.id)}
-
-  context 'viewing comments' do
-    before { Comment.create(text: 'This is a comment')}
-    scenario 'any user can see comments on a post' do
-      visit '/posts'
-      expect(page).to have_content 'This is a comment'
-    end
-  end
 
   context 'creating comments' do
+    before do
+      sign_up
+      post_photo 
+    end
+
+    scenario 'a logged in user can create a comment' do
+      visit '/posts'
+      click_link 'Add a comment'
+      fill_in 'Text', with: 'This is a comment' 
+      click_button 'Add a comment'
+      expect(page).to have_content 'This is a comment'
+    end
   end
 end
--- spec/helpers/user_helper_spec.rb
+++ spec/helpers/user_helper_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe UsersHelper, type: :helper  do
-  context '#email_to_username' do
+  context '#fetch_username' do
     it 'converts an email to a username given a user_id' do
       user = User.create(email: 'pjackson@gmail.com', password: 'password', password_confirmation: 'password')
-      expect(email_to_username user.id).to eq 'pjackson'
+      expect(fetch_username user.id).to eq 'pjackson'
     end
   end
 end
