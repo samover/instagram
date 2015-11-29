05fba3a2fa37aa9bd6ad35a4584cd5b9f2f7608c
samover <samuel.overloop@gmail.com>
2015-11-29 14:56:42 +0000

    add user id to likes

--- spec/features/likes_features_spec.rb
+++ spec/features/likes_features_spec.rb
@@ -4,22 +4,29 @@ describe 'features for liking a post' do
   before do
     user = create(:user)
     create(:post, user_id: user.id )
+    sign_up
+    visit '/posts'
   end
   
   context 'liking posts' do
     scenario 'there is a link to like a post' do
-      visit '/posts'
       within('div.likes') do
         expect(page).to have_link 'Like'
       end
     end
 
     scenario 'a user can like a post' do
-      sign_up
-      visit '/posts'
       click_link 'Like'
       within('div.likes') do
         expect(page).to have_content '1 like'
+      end
+    end
+
+    scenario 'a user can not like a post twice, but can unlike' do
+      click_link 'Like'
+      within('div.likes') do
+        expect(page).not_to have_link 'Like'
+        expect(page).to have_link 'Unlike'
       end
     end
   end
